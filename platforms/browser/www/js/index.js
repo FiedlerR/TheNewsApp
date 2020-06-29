a1336ce0bbcc62a31684265c27292234d94689e5
FiedlerR <6136@htl.rennweg.at>
2020-06-29 19:49:33 +0200

    -UI Upgrade- added new app logo resized category logos added UI Settings added all w3 themes changed all remote css dependencies to local added localstorage to save user settings added clear all settings button and functionality category Title is adaptive now Settings meu is open at the first start added loading screen added toast messages added deviceready event for app start

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -15,193 +15,283 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */  
+ */
+document.addEventListener("deviceready", appReady, false);
+var country = "us";
+var categories = ['general', 'entertainment', 'health', 'science', 'sports', 'technology', 'business'];
 
-var app = {
-    // Application Constructor
-    initialize: function () {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function () {
-        this.receivedEvent('deviceready');
-
-        //window.open = cordova.InAppBrowser.open;
-        var country = "de";
-        loadHeadLines(country);
+function appReady() {
+    loadSavedData();
+    setCategoryChips();
+    loadHeadLines(country);
+    setCategoryUI(categories);
 
 
-        /*
-        $('#optionBarButton').click(function (){
-            $("head link#theme").attr('href','https://www.w3schools.com/lib/w3-theme-green.css');
-         });
-         */
+    $("#newsOverviewSettingsCountry").val(country);
 
-        $("#newsOverviewSettingsCountry").val(country);
-        setAvailableChips();
+    //setAvailableChips();
 
 
-
-        $('.ui.dropdown')
-        .dropdown()
-      ;
-
-      $('.tag.example .ui.dropdown')
-        .dropdown({
-            allowAdditions: true
-        })
-        ;
+    document.getElementById("searchButton").addEventListener("click", searchRequest);
+    document.getElementById("saveNewsOverviewSettings").addEventListener("click", saveNewsOverviewSettings);
+    document.getElementById("saveUiSettings").addEventListener("click", saveUiSettings);
+    document.getElementById("clearAllSettings").addEventListener("click", clearALLSettings);
 
 
-        document.getElementById("searchButton").addEventListener("click", searchRequest);
-       // document.getElementById("optionBarButton").addEventListener("click", openOptionBar);
-        document.getElementById("saveNewsOverviewSettings").addEventListener("click", saveNewsOverviewSettings);
+    $(document).on('keypress', 'input', function (event) {
+        var keycode = event.keyCode || event.which;
+        if (keycode == '13') {
+            searchRequest();
+        }
+    });
 
-        
+    document.getElementById("loaderBar").style.display = "none";
+}
 
-        $(document).on('keypress', 'input', function(event) {   
-            var keycode = event.keyCode || event.which;
-            if(keycode == '13') {
-                searchRequest();
-            }
+function loadSavedData() {
+    var storage = window.localStorage;
+
+    if (storage.getItem("country") === null) {
+        storage.setItem("country", "us");
+    } else {
+        country = storage.getItem("country");
+    }
+
+    if (storage.getItem("categories") === null) {
+        storage.setItem("categories", JSON.stringify(categories));
+    } else {
+        categories = JSON.parse(storage.getItem("categories"));
+    }
+
+    if (storage.getItem("theme") === null) {
+        storage.setItem("theme", "black");
+        $("#uiSettingsTheme").val("black");
+    } else {
+        $('#theme').attr('href', 'css/w3_themes/w3-theme-' + storage.getItem("theme") + '.css');
+        $("#uiSettingsTheme").val(storage.getItem("theme"));
+    }
+
+
+    if (storage.getItem("firstStart") === null) {
+        storage.setItem("firstStart", "false");
+        w3_open();
+    }
+}
+
+function setCategoryChips() {
+    var availableCategories = ['general', 'entertainment', 'health', 'science', 'sports', 'technology', 'business'];
+    $.each(availableCategories, function (index, val) {
+        if (categories.includes(val)) {
+            w3_addChip(null, val);
+        } else {
+            w3_deleteChip(null, val);
+        }
+    });
+
+}
+
+function setAvailableChips() {
+    var availableCategories = ['general', 'entertainment', 'health', 'science', 'sports', 'technology', 'business'];
+    $.each(availableCategories, function (index, val) {
+        $('#availableChips').append('<button class="w3-btn w3-round-xxlarge w3-theme-l2 w3-margin-top w3-margin-right" onclick="w3_addChip(this,\'' + val + '\')")>' + val + '</button>');
+    });
+}
+
+function setCategoryUI(categories) {
+    $("#categoryArea").empty();
+    for (let index = 0; index < categories.length; index++) {
+        if (index == 0 || (index == 1 && categories.length % 2 == 0)) {
+            var firstCard = `
+                <div class="w3-card-4" onclick="changeToCategory('` + categories[index] + `')">
+                <div class="w3-display-container w3-text-white">
+                <img id="categoryImage0" src="img/categories/` + categories[index] + `.jpg" alt="Lights" style="width:100%">
+                <div id="categoryText0" class="w3-xlarge w3-display-bottomleft w3-padding">` + categories[index] + `</div>
+                </div>
+                </div>`;
+            $('#categoryArea').append(firstCard);
+
+        } else {
+
+            var index2 = index + 1;
+            var card = `
+                <div class="w3-cell-row">
+                <div class=" w3-cell">
+                <div class="w3-card-4 " onclick="changeToCategory('` + categories[index] + `')">
+                <div class="w3-display-container w3-text-white">
+                <img src="img/categories/` + categories[index] + `.jpg" alt="Lights" style="width:100%">
+                <div class="w3-large w3-display-bottomleft w3-padding">` + categories[index] + `</div>
+                </div>
+                </div>
+                </div>
+                <div class=" w3-cell">
+                <div class="w3-card-4 " onclick="changeToCategory('` + categories[index + 1] + `')">
+                <div class="w3-display-container w3-text-white">
+                <img src="img/categories/` + categories[index + 1] + `.jpg" alt="Lights" style="width:100%">
+                <div class="w3-large w3-display-bottomleft w3-padding">` + categories[index + 1] + `</div>
+                </div>
+                </div>
+                </div>
+                </div>`
+            $('#categoryArea').append(card);
+            index++;
+        }
+    }
+}
+
+function changeToCategory(category) {
+    hideSettingsButton();
+    $("#categoryArea").empty();
+    loadCategoryHeadlines(category);
+}
+
+function backToNewsOverview() {
+    hideBackButton();
+    loadHeadLines(country);
+    setCategoryUI(categories);
+}
+
+function clearALLSettings(){
+    localStorage.clear();
+}
+
+function saveNewsOverviewSettings() {
+    country = $("#newsOverviewSettingsCountry").val();
+    loadHeadLines(country);
+    var categoriesTemp = Array();
+    $("#selectedChips :button").each(function () {
+        categoriesTemp.push($(this).text());
+    });
+    categories = categoriesTemp;
+    setCategoryUI(categories);
+
+    var storage = window.localStorage;
+    storage.setItem("country", country);
+    storage.setItem("categories", JSON.stringify(categories));
+    showToast("News Overview Settings saved");
+}
+
+function saveUiSettings() {
+    var storage = window.localStorage;
+    theme = $("#uiSettingsTheme").val();
+    $('#theme').attr('href', 'css/w3_themes/w3-theme-' + theme + '.css');
+    storage.setItem("theme", theme);
+    showToast("UI Settings saved");
+}
+
+function buildNewsFeed(data,category) {
+    $("#newsFeed").empty();
+    $.each(data.articles, function (index, element) {
+
+        var itemDiv = $("<div />", {
+            "class": "w3-card-4 w3-margin w3-animate-bottom",
         });
 
-        function openOptionBar (){
-            $('.ui.sidebar')
-            .sidebar('toggle')
-          ;
+        var imageDiv = $("<div />", {
+            "style": "width:100%",
+            "class": ""
+        });
+
+
+        var imageTag = $("<img />", {
+            "style": "width:100%",
+            src: element.urlToImage
+        });
+
+        imageDiv.append(imageTag);
+
+        itemDiv.append(imageDiv);
+
+        var contentDiv = $("<div />", {
+            "class": "w3-container"
+        });
+
+        var newsHeader = $("<a />", {
+            "class": "cardTitle",
+            text: element.title,
+            onClick: "cordova.InAppBrowser.open('" + element.url + "', '_blank', 'location=yes zoom=no');"
+        });
+
+        var sourceDiv = $("<div />", {
+            "class": "",
+            text: element.source.name
+        });
+
+        var publishDateDiv = $("<div />", {
+            "class": "",
+            text: element.publishedAt
+        });
+
+
+        itemDiv.append(imageDiv);
+
+        contentDiv.append(sourceDiv);
+        contentDiv.append(newsHeader);
+        contentDiv.append(publishDateDiv);
+        itemDiv.append(contentDiv);
+
+        if(category === null){
+            $('#topHeadlines').text("Top Headlines");
+        }else{
+            console.log("!!!!!!!!!!!!!!!!!!");
+            $('#topHeadlines').text("Top "+category+" Headlines");
         }
+        $('#newsFeed').append(itemDiv);
 
-        function setAvailableChips(){
-            var categories = ['general', 'entertainment', 'health', 'science','sports','technology'];
-            $.each(categories , function(index, val) { 
-                $('#availableChips').append('<button class="w3-btn w3-round-xxlarge w3-theme-l2 w3-margin-top w3-margin-right" onclick="w3_addChip(this,\''+val+'\')")>'+val+'</button>');
-            });
+    });
+}
+
+function loadHeadLines(country) {
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=***REMOVED***',
+        data: {
+            get_param: 'value'
+        },
+        dataType: 'json',
+        crossDomain: true,
+        success: function (data) {
+            buildNewsFeed(data, null);
         }
+    });
+}
 
-   
+function searchRequest() {
+    var searchText = $("#searchField").val();
+    console.log(searchText);
 
-        function saveNewsOverviewSettings(){
-            country = $("#newsOverviewSettingsCountry").val();
-            loadHeadLines(country);
-        }
+    if (searchText !== "" || searchText) {
+        $.ajax({
+            type: 'GET',
+            url: 'http://newsapi.org/v2/everything?q=' + searchText + '&language=de&apiKey=***REMOVED***',
+            data: {
+                get_param: 'value'
+            },
+            dataType: 'json',
+            crossDomain: true,
+            success: function (data) {
 
-        function newFunction(data) {
-            $("#newsFeed").empty();
-            $.each(data.articles, function (index, element) {
-
-                var itemDiv = $("<div />", {
-                    "class": "w3-card-4 w3-margin w3-animate-bottom",
-                });
-
-                var imageDiv = $("<div />", {
-                    "style": "width:100%",
-                    "class": ""
-                });
-
-
-                var imageTag = $("<img />", {
-                    "style": "width:100%",
-                    src: element.urlToImage
-                });
-
-                imageDiv.append(imageTag);
-
-                itemDiv.append(imageDiv);
-
-                var contentDiv = $("<div />", {
-                    "class": "w3-container"
-                });
-
-                var newsHeader = $("<a />", {
-                    "class": "cardTitle",
-                    text: element.title,
-                    onClick: "cordova.InAppBrowser.open('" + element.url + "', '_blank', 'location=yes zoom=no');"
-                });
-
-                var sourceDiv = $("<div />", {
-                    "class": "",
-                    text: element.source.name
-                });
-
-                var publishDateDiv = $("<div />", {
-                    "class": "",
-                    text: element.publishedAt
-                });
-
-
-                itemDiv.append(imageDiv);
-
-                contentDiv.append(sourceDiv);
-                contentDiv.append(newsHeader);
-                contentDiv.append(publishDateDiv);
-                itemDiv.append(contentDiv);
-
-                $('#newsFeed').append(itemDiv);
-
-            });
-        }
-
-        function loadHeadLines(country) {
-
-            $.ajax({
-                type: 'GET',
-                url: 'http://newsapi.org/v2/top-headlines?country='+country+'&apiKey=***REMOVED***',
-                data: {
-                    get_param: 'value'
-                },
-                dataType: 'json',
-                crossDomain: true,
-                success: function (data) {
-
-                    newFunction(data);
-                }
-            });
-        }
-
-        function searchRequest() {
-            var searchText = $("#searchField").val();
-            console.log(searchText);
-
-            if(searchText !== "" || searchText){
-                $.ajax({
-                    type: 'GET',
-                    url: 'http://newsapi.org/v2/everything?q='+searchText+'&language=de&apiKey=***REMOVED***',
-                    data: {
-                        get_param: 'value'
-                    },
-                    dataType: 'json',
-                    crossDomain: true,
-                    success: function (data) {
-    
-                        newFunction(data);
-                    }
-                });
-                loadHeadLines(country);
+                buildNewsFeed(data);
             }
-
-
-
-        }
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        /*
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    */
+        });
+    } else {
+        loadHeadLines(country);
     }
-};
+}
 
-app.initialize();
+
+function loadCategoryHeadlines(category) {
+    $.ajax({
+        type: 'GET',
+        url: 'http://newsapi.org/v2/top-headlines?country=' + country + '&category=' + category + '&apiKey=***REMOVED***',
+        data: {
+            get_param: 'value'
+        },
+        dataType: 'json',
+        crossDomain: true,
+        success: function (data) {
+
+            buildNewsFeed(data,category);
+        }
+    });
+}
--- platforms/browser/www/w3CSS/w3CSS.js
+++ platforms/browser/www/w3CSS/w3CSS.js
@@ -7,15 +7,22 @@ function w3_open() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
   }
+  
 
   function w3_addChip(avaiableChip,name) {
-    avaiableChip.remove(); 
+    if(avaiableChip !== null){
+      avaiableChip.remove(); 
+    }
+
     var chip = '<button class="w3-btn w3-round-xxlarge w3-theme-l2 w3-margin-top w3-margin-right" onclick="w3_deleteChip(this,\''+name+'\')">'+name+'</button>'
     document.getElementById("selectedChips").insertAdjacentHTML( 'beforeend', chip );
   }
 
   function w3_deleteChip(selectedChip,name) {
-    selectedChip.remove(); 
+    if(selectedChip !== null){
+      selectedChip.remove(); 
+    }
+
     var chip = '<button class="w3-btn w3-round-xxlarge w3-theme-l2 w3-margin-top w3-margin-right" onclick="w3_addChip(this,\''+name+'\')")>'+name+'</button>'
     document.getElementById("availableChips").insertAdjacentHTML( 'beforeend', chip );
   }
@@ -24,4 +31,24 @@ function w3_open() {
   document.addEventListener("backbutton", onBackKeyDown, false);
   function onBackKeyDown() {
     w3_close();
+  }
+
+
+  function hideSettingsButton (){
+    document.getElementById("optionBarButton").style.display = "none";
+    document.getElementById("backButton").style.display = "block";
+  }
+
+  hideBackButton ();
+  function hideBackButton (){
+    document.getElementById("optionBarButton").style.display = "block";
+    document.getElementById("backButton").style.display = "none";
+  }
+
+
+  function showToast(message) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerHTML = message;
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
   }
